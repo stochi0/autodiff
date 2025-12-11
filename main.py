9d38bc165a20f9d4218560495641b52773a3c5b8
stochi0 <233351902+stochi0@users.noreply.github.com>
2025-12-11 14:25:42 +0530

    Add graphviz support

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from src.nn import Value
+from src.viz import draw_dot
 
 def main():
     print("Hello from autodiff!")
-    a = Value(2.0)
-    b = Value(3.0)
+    a = Value(2.0, label='a')
+    b = Value(3.0, label='b')
     c = a + b
     print(c)
-
+    dot = draw_dot(c)
+    dot.render('graph', format='svg', cleanup=True)
 
 if __name__ == "__main__":
     main()
--- src/nn.py
+++ src/nn.py
@@ -1,11 +1,12 @@
 class Value:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self._op = _op
         self._children = set(_children)
+        self.label = label
 
     def __repr__(self):
-        return f"Value(data={self.data}, _children={self._children}, _op={self._op})"
+        return f"Value(data={self.data}, _children={self._children}, _op={self._op}, label={self.label})"
 
     def __add__(self, other):
         return Value(self.data + other.data, (self, other), '+')
